1d1f9e338158404fc95b819b7fd47edf9a779aa7
Sufyan Ali <msufyan.202105869@gcuf.edu.pk>
2026-07-29 11:48:53 +0500

    feat: create user mongoose model with authentication and role fields

---
+++ src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true        
+    },
+    password: { 
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'customer'],
+        default: 'customer'
+    },
+    resetOtp: {
+        type: String,
+        default: ''
+    },
+    resetOtpExpireAt: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+export default User;
