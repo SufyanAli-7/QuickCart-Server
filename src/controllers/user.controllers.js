c79673a2a02ef6cef0913608228d0374c82b8969
Sufyan Ali <msufyan.202105869@gcuf.edu.pk>
2026-07-30 10:32:24 +0500

    feat: implement user management routes and controller methods for profile retrieval and admin access

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 
-export const getUserData = async (req, res) => {
+export const getCurrentUser = async (req, res) => {
     try {
         const id = req.id;
         
@@ -30,6 +30,31 @@ export const getAllUsers = async (req, res) => {
         const users = await User.find({}).select("-password");
 
         return res.status(200).json({success: true, users});
+    }
+    catch (error) {
+        console.error('Error during logout:', error.message);
+        res.status(500).json({success: false, message: 'Internal server error'});
+    }
+}
+
+
+export const getUserById = async (req, res) => {
+    try {
+        const { id, role } = req;
+
+        if (role !== 'admin' || !id) {
+            return res.status(403).json({success: false, message: 'Forbidden'});
+        }
+
+        const userId = req.params.id;
+
+        const user = await User.findById(userId).select("-password");
+        
+        if (!user) {
+            return res.status(404).json({success: false, message: 'User not found'});
+        }
+
+        return res.status(200).json({success: true, user});
     }
     catch (error) {
         console.error('Error during logout:', error.message);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { getAllUsers, getUserData } from "../controllers/user.controllers.js";
+import { getAllUsers, getCurrentUser, getUserById } from "../controllers/user.controllers.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 // All Users Can Access
-userRouter.get('/data', authMiddleware, getUserData);
+userRouter.get('/current', authMiddleware, getCurrentUser);
 
 // Admin Only
 userRouter.get('/all', authMiddleware, getAllUsers);
 
+userRouter.get('/:id', authMiddleware, getUserById);
 
 export default userRouter;
